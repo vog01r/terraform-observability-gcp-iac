858dfa1188ec4c7f3f0a50cacf86826bee3b716c
vogeaux <vogeaux@gmail.com>
2025-10-09 10:27:29 +0000

    Suppression de toutes les restrictions réseau et firewall
    - Supprimé toutes les règles firewall dans Terraform (SSH, HTTP/HTTPS, trafic interne, NAT)
    - Supprimé la configuration NAT et les routes par défaut
    - Ajouté des IPs publiques pour toutes les VMs (k8s, obs, app)
    - Supprimé la configuration NAT et iptables dans Ansible
    - Supprimé les configurations de routage via gateway
    - Mis à jour l'inventaire Ansible pour utiliser les IPs publiques
    - Supprimé le proxy SSH via bastion - accès direct aux VMs
    
    Toutes les VMs ont maintenant des IPs publiques sans restrictions réseau.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,24 +5,24 @@ output "bastion_public_ip" {
   }
 }
 
-output "k8s_private_ip" {
+output "k8s_public_ip" {
   value = {
     for uid, instance in google_compute_instance.k8s :
-    uid => instance.network_interface[0].network_ip
+    uid => instance.network_interface[0].access_config[0].nat_ip
   }
 }
 
-output "obs_private_ip" {
+output "obs_public_ip" {
   value = {
     for uid, instance in google_compute_instance.obs :
-    uid => instance.network_interface[0].network_ip
+    uid => instance.network_interface[0].access_config[0].nat_ip
   }
 }
 
-output "app_private_ip" {
+output "app_public_ip" {
   value = {
     for uid, instance in google_compute_instance.app :
-    uid => instance.network_interface[0].network_ip
+    uid => instance.network_interface[0].access_config[0].nat_ip
   }
 }
 
@@ -32,6 +32,11 @@ output "import_command" {
 }
 
 output "instance_ip" {
-  value       = { for uid, instance in google_compute_instance.gw : uid => instance.network_interface[0].network_ip }
-  description = "Adresses IP internes des instances"
+  value       = merge(
+    { for uid, instance in google_compute_instance.gw : uid => instance.network_interface[0].access_config[0].nat_ip },
+    { for uid, instance in google_compute_instance.k8s : uid => instance.network_interface[0].access_config[0].nat_ip },
+    { for uid, instance in google_compute_instance.obs : uid => instance.network_interface[0].access_config[0].nat_ip },
+    { for uid, instance in google_compute_instance.app : uid => instance.network_interface[0].access_config[0].nat_ip }
+  )
+  description = "Adresses IP publiques de toutes les instances"
 }
